157a4ebe26b68cc660d712f1b7e7eb209d451826
FridaSvanberg <svanberg92@gmail.com>
2025-05-28 13:12:32 +0200

    Add placeholder bg

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ import Traits from './components/Traits';
 
 function App() {
   return (
-    <main className="min-h-screen bg-gray-900 text-gray-100 p-6 font-mono">
+    // <main className="min-h-screen bg-gray-90 text-gray-100 p-6 font-mono">
+    <main className="min-h-screen bg-gray-900 text-gray-100 font-mono bg-[url('/bg-placeholder.png')] bg-cover bg-center bg-no-repeat">
+      {/* <div className="min-h-screen bg-[url('/bg-texture.png')] bg-cover bg-center text-gray-100 font-mono"> */}
       <div className="max-w-3xl mx-auto bg-gray-800 border-4 border-yellow-500 rounded-xl shadow-xl p-6 space-y-6">
         <Header />
         <Stats />
